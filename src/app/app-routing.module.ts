da016e483903863d6fe412d7e2af44573b576647
CapBenjamin09 <sanchez7888555@gmail.com>
2023-09-18 12:49:24 -0600

    feat: folder components is added and restructuring of files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from "./components/login/login.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  //Rutas
+  { path: 'login', component: LoginComponent},
+
+  //Ruta por defecto
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
